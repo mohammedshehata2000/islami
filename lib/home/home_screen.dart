306a2f4d75d60795d5a82d09341abab450a0627b
mohammed shehata <mshehata10011001@gmail.com>
2024-08-02 09:48:10 +0300

    add quran tab. add hadith tab. add radio tab. add sebha tab. add settings tab. add sura tab. add hadith deletes tab. add theme tab. add language tab.

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,10 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islamii/home/hadeth/hadeth_tab.dart';
+import 'package:islamii/home/quran/quran_tab.dart';
+import 'package:islamii/home/radio/radio_tab.dart';
+import 'package:islamii/home/sebha/sebha_tab.dart';
+import 'package:islamii/home/settinges/settinges_tab.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../providers/app_config_provider.dart';
+
+class HomeScreen extends StatefulWidget {
   static const String routeName = 'homeScreen';
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var provider = Provider.of<AppConfigProvider>(context);
+    return Stack(children: [
+      !(provider.isDark())
+          ? Image.asset(
+              'assets/images/bgl.png',
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.fill,
+            )
+          : Image.asset(
+              'assets/images/bgd.png',
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.fill,
+            ),
+      Scaffold(
+        appBar: AppBar(
+          title: Text(
+            AppLocalizations.of(context)!.islami,
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        ),
+        bottomNavigationBar: Theme(
+          data: Theme.of(context)
+              .copyWith(canvasColor: Theme.of(context).primaryColor),
+          child: BottomNavigationBar(
+            currentIndex: selectedIndex,
+            onTap: (index) {
+              selectedIndex = index;
+              setState(() {});
+            },
+            items: [
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage('assets/images/quran.png')),
+                  label: AppLocalizations.of(context)!.quran),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage('assets/images/hadeth.png')),
+                  label: AppLocalizations.of(context)!.hadeth),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage('assets/images/radio.png')),
+                  label: AppLocalizations.of(context)!.radio),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage('assets/images/sebha.png')),
+                  label: AppLocalizations.of(context)!.sebha),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  label: AppLocalizations.of(context)!.settings),
+            ],
+          ),
+        ),
+        body: tapList[selectedIndex],
+      ),
+    ]);
   }
+
+  List<Widget> tapList = [
+    QuranTab(),
+    HadethTab(),
+    RadioTab(),
+    SebhaTab(),
+    SettingesTab()
+  ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islamii/home/hadeth/hadeth_detales.dart';
 import 'package:islamii/home/home_screen.dart';
+import 'package:islamii/home/quran/sura_detales.dart';
+import 'package:islamii/my_theme_data.dart';
+import 'package:islamii/providers/app_config_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => AppConfigProvider(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<AppConfigProvider>(context);
+    // var provider = Provider.of<AppConfigProvider>(context) ;
     return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: Locale(provider.appLanguge),
+      themeMode: provider.appTheme,
+      theme: MyThemeData.lightMode,
+      darkTheme: MyThemeData.darkMode,
       debugShowCheckedModeBanner: false,
       initialRoute: HomeScreen.routeName,
       routes: {
         HomeScreen.routeName: (context) => HomeScreen(),
+        SuraDetales.routeName: (context) => SuraDetales(),
+        HadethDetales.routeName: (context) => HadethDetales(),
       },
     );
   }
